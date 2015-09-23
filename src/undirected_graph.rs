37fa0bd7156558cd8d76a59f125ba468906830f7
Paul Dib <paul.dib@orange.fr>
2015-09-23 20:55:33 +0200

    Add dfsiterator

---
+++ src/undirected_graph.rs
@@ -0,0 +1,123 @@
+pub struct UndirectedGraph<T> {
+    edges: Vec<Vec<usize>>,
+    data: Vec<Option<T>>
+}
+
+pub struct DfsIterator<'a, T: 'a> {
+    graph: &'a mut UndirectedGraph<T>,
+    stack: Vec<usize>,
+    visited: Vec<bool>
+}
+
+impl<'a, T: Clone> DfsIterator<'a, T> {
+    pub fn new(graph: &'a mut UndirectedGraph<T>, start: usize) -> DfsIterator<'a, T> {
+        let size = graph.size();
+        let mut it = DfsIterator {
+            graph: graph,
+            stack: vec![start],
+            visited: Vec::with_capacity(size)
+        };
+        for i in 0..size{
+            it.visited[i] = i == start;
+        }
+        it
+    }
+}
+
+impl<'a, T: Clone> Iterator for DfsIterator<'a, T> {
+    type Item = T;
+    
+    fn next(&mut self) -> Option<T> {
+        if self.stack.is_empty() {
+            None
+        } else {
+            let mut next_node = self.stack[self.stack.len() - 1];
+            let mut found = true;
+            while found {
+                found = false;
+                for v in self.graph.from(next_node) {
+                    if !self.visited[*v] {
+                        found = true;
+                        next_node = *v;
+                        self.visited[*v] = true;
+                        self.stack.push(*v);
+                        break;
+                    }
+                }
+            }
+            self.stack.pop();
+            match self.graph.get_data(next_node) {
+                Some(t) => Some(t.clone()),
+                _ => None
+            }
+        }
+    }
+}
+
+impl<T: Clone> UndirectedGraph<T> {
+    pub fn new(size: usize) -> UndirectedGraph<T> {
+        let mut s = UndirectedGraph {
+            edges: Vec::with_capacity(size),
+            data: Vec::with_capacity(size)
+        };
+        for _ in 0..size {
+            s.edges.push(Vec::new());
+            s.data.push(None);
+        }
+        s
+    }
+
+    pub fn set_data(&mut self, u: usize, t: T) {
+        self.data[u] = Some(t);
+    }
+
+    pub fn get_data(&mut self, u: usize) -> Option<&T> {
+        self.data[u].as_ref()
+    }
+    
+    pub fn add_edge(&mut self, u: usize, v: usize) {
+        self.edges[u].push(v);
+        self.edges[v].push(u);
+    }
+    
+    pub fn from(&self, u: usize) -> &Vec<usize> {
+        &self.edges[u]
+    }
+
+    pub fn size(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn dfs(&mut self, start: usize) -> DfsIterator<T> {
+        DfsIterator::new(self, start)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::UndirectedGraph;
+
+    #[test]
+    fn simple_graph () {
+        let mut s = UndirectedGraph::<i32>::new(3);
+        s.add_edge(0, 1);
+        s.add_edge(1, 2);
+        assert_eq!(s.from(0), &[1]);
+        assert_eq!(s.from(1), &[0, 2]);
+        assert_eq!(s.from(2), &[1]);
+    }
+
+    #[test]
+    fn with_data () {
+        let mut s = UndirectedGraph::<i32>::new(2);
+        s.set_data(0, 1337);
+        assert_eq!(*s.get_data(0).unwrap(), 1337);
+        assert!(s.get_data(1).is_none());
+    }
+
+    #[test]
+    fn depth_first_search () {
+        
+    }
+}
+
